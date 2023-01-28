17b3e74c3dab84fda7fe022008be18598c4f13f3
James Solomon <solomonjames@gmail.com>
2023-01-28 17:08:24 -0500

    just removing some docblock voids for real ones

--- database/seeders/ShortUrlSeeder.php
+++ database/seeders/ShortUrlSeeder.php
@@ -9,10 +9,8 @@ class ShortUrlSeeder extends Seeder
 {
     /**
      * Run the database seeds.
-     *
-     * @return void
      */
-    public function run()
+    public function run(): void
     {
         ShortUrl::factory()
             ->count(100)
