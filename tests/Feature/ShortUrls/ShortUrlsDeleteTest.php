432af4ec05b3fe5f3205085f90006d9259094130
James Solomon <solomonjames@gmail.com>
2023-01-31 12:11:25 -0500

    Running linter

--- tests/Feature/ShortUrls/ShortUrlsDeleteTest.php
+++ tests/Feature/ShortUrls/ShortUrlsDeleteTest.php
@@ -11,6 +11,7 @@ class ShortUrlsDeleteTest extends TestCase
     use RefreshDatabase;
 
     private const DELETE_URL = '/api/short-urls/';
+
     private const ARCHIVED_INDEX_URL = '/api/archived-short-urls';
 
     public function test_expect_no_content_when_successfully_deleted()
@@ -18,7 +19,7 @@ class ShortUrlsDeleteTest extends TestCase
         /** @var ShortUrl $shortUrl */
         $shortUrl = ShortUrl::factory()->create();
 
-        $response = $this->deleteJson(self::DELETE_URL . $shortUrl->short_code);
+        $response = $this->deleteJson(self::DELETE_URL.$shortUrl->short_code);
 
         $response->assertStatus(204);
     }
@@ -28,7 +29,7 @@ class ShortUrlsDeleteTest extends TestCase
         /** @var ShortUrl $shortUrl */
         $shortUrl = ShortUrl::factory()->create();
 
-        $this->deleteJson(self::DELETE_URL . $shortUrl->short_code);
+        $this->deleteJson(self::DELETE_URL.$shortUrl->short_code);
 
         $response = $this->getJson(self::ARCHIVED_INDEX_URL);
         $response->assertJsonPath('data.0.short_code', $shortUrl->short_code);
