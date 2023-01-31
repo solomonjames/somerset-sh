e5564fabbd48c14c7197fe7b6cd488a4d033f682
James Solomon <solomonjames@gmail.com>
2023-01-31 11:44:36 -0500

    Adding a test for ShortUrl creation

---
+++ tests/Feature/ShortUrls/ShortUrlsCreateTest.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace Tests\Feature\ShortUrls;
+
+use App\Models\ShortUrl;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Testing\Fluent\AssertableJson;
+use Tests\TestCase;
+
+class ShortUrlsCreateTest extends TestCase
+{
+    use RefreshDatabase;
+
+    private const CREATE_URL = '/api/short-urls';
+
+    public function test_creating_valid_short_url()
+    {
+        $response = $this->postJson(self::CREATE_URL, ['long_url' => 'https://google.com']);
+
+        $response->assertStatus(201);
+    }
+
+    public function test_expect_long_url_error_when_passing_invalid_scheme()
+    {
+        $response = $this->postJson(self::CREATE_URL, ['long_url' => 'ws://google.com']);
+
+        $response->assertStatus(422)
+            ->assertJson(fn (AssertableJson $json) =>
+                $json->has('message')
+                    ->has('errors.long_url')
+            );
+    }
+
+    public function test_expect_required_error_when_missing_long_url()
+    {
+        $response = $this->postJson(self::CREATE_URL);
+
+        $response->assertStatus(422)
+            ->assertJson(fn (AssertableJson $json) =>
+                $json->has('message')
+                    ->has('errors.long_url')
+            );
+    }
+
+    public function test_expect_length_error_when_long_url_is_too_long()
+    {
+        $response = $this->postJson(self::CREATE_URL, [
+            'long_url' => str('http://google.com/?test=')->padRight(600, 'p')->toString()
+        ]);
+
+        $response->assertStatus(422)
+            ->assertJson(fn (AssertableJson $json) =>
+                $json->has('message')
+                    ->has('errors.long_url')
+            );
+    }
+
+    public function test_expect_no_creation_when_passing_existing_long_url()
+    {
+        $initialCreation = $this->postJson(self::CREATE_URL, ['long_url' => 'https://google.com']);
+        $initialCreation->assertStatus(201);
+
+        $response = $this->postJson(self::CREATE_URL, ['long_url' => 'https://google.com']);
+
+        $response->assertStatus(200);
+    }
+}
